b139961cb4646fe16dc3d0629bd6b8cb5ae04881
Randy Chang <rchang@ebates.com>
2019-11-01 15:51:52 -0700

    - Fixed Bug - Updated code to use modern base64 conversion library - Added tests - Updated markdown

---
+++ src/main.js
@@ -0,0 +1,92 @@
+const ab2b64 = require("ab2b64");
+
+class OneTimePad {
+    #key = null;
+
+    constructor(key, options=null) {
+        //helper functions to convert Array Buffer to String and vice versa, for encrypting and decrypting string
+        this.ab2str = this.ab2str.bind(this);
+        this.str2ab = this.str2ab.bind(this);
+
+        //Main encryption function
+        this.pad = this.pad.bind(this);
+
+        //Main string encryption functions
+        this.encryptString = this.encryptString.bind(this);
+        this.decryptString = this.decryptString.bind(this);
+
+        //Accept key as both string and array buffer
+        if(typeof(key) == 'string') {
+            this.#key = this.str2ab(key);
+        } else if(key instanceof ArrayBuffer) {
+            this.#key = key;
+        } else {
+            throw new Error("key has to be either string or ArrayBuffer");
+        }
+
+        //encryption options, unused at the moment
+        this.options = options;
+    }
+
+    //Main padding function
+    //offset is the starting character of the key for encryption
+    //step is the number of characters in the key to step through for each character at plaintext
+    pad(payloadBuffer, offset=0, step=1) {
+        return new Promise((resolve, reject) => {
+            if(payloadBuffer.byteLength > (this.#key.byteLength * step + offset)) {
+                reject("Text size is bigger than usable key size!  Key size has to be equal or greater than (Text size * step + offset)")
+            } else {
+                let keyView = new Uint8Array(this.#key);
+                let textView = new Uint8Array(payloadBuffer);
+                let encodedBuffer = new ArrayBuffer(payloadBuffer.byteLength);
+                let encodedView = new Uint8Array(encodedBuffer);
+
+                for(let index = 0; index < textView.length; index++) {
+                    encodedView[index] = keyView[index * step + offset] ^ textView[index];
+                }
+                resolve(encodedBuffer)
+            }
+        });
+    }
+
+    encryptString(str, offset=0, step=1) {
+        return new Promise((resolve, reject) => {
+            let buffer = this.str2ab(str);
+
+            this.pad(buffer, offset, step).then(encodedBuffer => {
+                resolve(ab2b64.ab2b64(encodedBuffer));
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    decryptString(str, offset=0, step=1) {
+        return new Promise((resolve, reject) => {
+            let buffer = ab2b64.b642ab(str);
+
+            this.pad(buffer, offset, step).then(encodedBuffer => {
+                resolve(this.ab2str(encodedBuffer));
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+    //functions taken from Renato Mangini on Google developer forum
+    ab2str(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
+    str2ab(str) {
+        let buf = new ArrayBuffer(str.length*2); //2 bytes for each char for 16 bits, supporting UTF-16
+        let bufView = new Uint16Array(buf);
+        for (let i=0, strLen=str.length; i < strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
+    }
+}
+
+module.exports = OneTimePad;
+
+//🔥🔥🔥🔥🔥🔥🔥🔥✨✨⚠️
--- test/test.js
+++ test/test.js
@@ -1,8 +1,29 @@
 const assert = require("assert");
-
+const OTP = require("../");
 
 const main_test = async () => {
+    const otp = new OTP(`Listen, Morty, I hate to break it to you but what people call "love" is just a chemical 
+    reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, 
+    leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. 
+    Break the cycle, Morty. Rise above. Focus on science`);
 
+    const plaintext1 = 'Weddings are basically funerals with cake.';
+    const ciphertext1 = await otp.encryptString(plaintext1, 3, 2);
+    assert(ciphertext1 === 'VwBlAGQAZABpAG4AZwBzACAAYQByAGUAIABiAGEAcwBpAGMAYQBsAGwAeQAgAGYAdQBuAGUAcgBhAGwAcwAgAHcAaQB0AGgAIABjAGEAawBlAC4A');
+
+    const plaintext2 = 'What about the reality where Hitler cured cancer, Morty? The answer is: Don\'t think about it.';
+    const ciphertext2 = await otp.encryptString(plaintext2, 5, 1);
+    assert(ciphertext2 === 'V3RoZWFudCwgIGFNYm9vcnV0dHkgLHQgaEllICBocmFldGFlbCBpdHRveSAgYndyaGVlYXJrZSAgaUh0aSB0dGxvZSByeSBvY3V1IHJiZXVkdCAgY3dhaG5hY3RlIHJwLGUgb01wb2xyZXQgeWM/YSBsVGxoIGUiIGxhb252c2V3ImUgcmkgc2kgc2o6dSBzRHRvIG5hJyB0YyBodGVobWlpbmNrYSBsYSBiCm8gdSB0ICAgaXJ0ZS5h');
+
+    const plaintext3 = 'I turned myself into a pickle. I\'m Pickle Riiiiick.';
+    const ciphertext3 = await otp.encryptString(plaintext3, 10, 2);
+    const decrypteed_plaintext = await otp.decryptString(ciphertext3, 10, 2);
+    assert(decrypteed_plaintext === plaintext3);
+
+    const plaintext4 = 'They’re just robots, Morty! It’s OK to shoot them! They’re robots!';
+    const ciphertext4 = await otp.encryptString(plaintext4, 5,1);
+    const incorrectly_decrypted_text = await otp.decryptString(ciphertext4, 3, 2);
+    assert(plaintext4 !== incorrectly_decrypted_text);
 };
 
 main_test().then(() => console.log("All tests pass!")).catch(err => console.log(err));
